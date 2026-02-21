2cbe51053c71012a74980ab5bd681b5d45fb7ca8
blessona12 <131678256+blessona12@users.noreply.github.com>
2026-02-21 20:48:21 +0200

    refactor: extract book price constant

--- bookstore/src/main/java/com/kata/bookstore/service/PricingService.java
+++ bookstore/src/main/java/com/kata/bookstore/service/PricingService.java
@@ -6,7 +6,9 @@ import java.util.Map;
 
 public class PricingService {
 
+    private static final double BOOK_PRICE = 50.0;
+
     public double calculateTotal(int quantity) {
-        return quantity*50.0;
+        return quantity*BOOK_PRICE;
     }
 }
