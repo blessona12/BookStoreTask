e131953b69d4ec41e07cb1fe3138beb1bfd86765
blessona12 <131678256+blessona12@users.noreply.github.com>
2026-02-21 21:39:44 +0200

    feat: support cart based pricing using map and handle null cart test

--- bookstore/src/main/java/com/kata/bookstore/service/PricingService.java
+++ bookstore/src/main/java/com/kata/bookstore/service/PricingService.java
@@ -1,14 +1,26 @@
 package com.kata.bookstore.service;
 
+import com.kata.bookstore.common.PricingConstants;
 import com.kata.bookstore.domain.BookType;
 
+import java.math.BigDecimal;
 import java.util.Map;
 
 public class PricingService {
 
     private static final double BOOK_PRICE = 50.0;
 
-    public double calculateTotal(int quantity) {
-        return quantity*BOOK_PRICE;
+    public double calculateTotal(Map<String, Integer> cart) {
+
+        if (cart == null || cart.isEmpty()) {
+            return 0.0;
+        }
+
+        int totalQuantity = cart.values()
+                .stream()
+                .mapToInt(Integer::intValue)
+                .sum();
+
+        return totalQuantity * BOOK_PRICE;
     }
 }
--- bookstore/src/test/java/com/kata/bookstore/service/PricingServiceTest.java
+++ bookstore/src/test/java/com/kata/bookstore/service/PricingServiceTest.java
@@ -1,7 +1,9 @@
 package com.kata.bookstore.service;
 
+import com.kata.bookstore.domain.BookType;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -11,12 +13,24 @@ class PricingServiceTest {
     private final PricingService pricingService = new PricingService();
 
     @Test
-    void shouldReturnZeroWhenCartIsEmpty() {
-        assertEquals(0.0, pricingService.calculateTotal(0));
+    void shouldReturnZeroWhenCartIsNull() {
+        assertEquals(0.0, pricingService.calculateTotal((Map<String, Integer>) null));
     }
 
     @Test
     void shouldCalculatePriceForSingleBook() {
-        assertEquals(50.0, pricingService.calculateTotal(1));
+        Map<String, Integer> cart =
+                Map.of("CLEAN_CODE", 1);
+        assertEquals(50.0, pricingService.calculateTotal(cart));
+    }
+
+    @Test
+    void shouldCalculateSubtotalForMultipleBooks() {
+        Map<String, Integer> cart = Map.of(
+                "CLEAN_CODE", 2,
+                "CLEAN_CODER", 1
+        );
+
+        assertEquals(150.0, pricingService.calculateTotal(cart));
     }
 }
